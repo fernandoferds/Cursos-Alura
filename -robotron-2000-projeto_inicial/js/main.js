e4393d65a91fba56c42cee2028be5898333463be
fernandoferds <ferdshigor@gmail.com>
2023-02-06 10:12:35 -0300

    Robotron finalizado

--- -robotron-2000-projeto_inicial/js/main.js
+++ -robotron-2000-projeto_inicial/js/main.js
@@ -1,19 +1,71 @@
 const subtrair = document.querySelector("#subtrair")
 const somar = document.querySelector("#somar")
-const braco = document.querySelector("#braco")
+const controle = document.querySelectorAll("[data-controle]")
+const estatistica = document.querySelectorAll("[data-estatistica]")
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
 
-somar.addEventListener("click", () => {
-   manipulaDados("somar")
-})
-
-subtrair.addEventListener("click", () => {
-    manipulaDados("subtrair")
-})
-
-function manipulaDados(operacao) {
-    if(operacao === "subtrair")
-    braco.value = parseInt(braco.value) - 1
- else {
-    braco.value = parseInt(braco.value) + 1
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
     }
+}
+
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+        atualizaEstatisticas(evento.target.dataset.pecas)
+    }) 
+})
+
+/* parseInt me permite trabalhar com numeros interios em vez de ser a string */
+
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector("[data-contador]")
+    
+    if(operacao === "-"){
+    peca.value = parseInt(peca.value) - 1}
+ else {
+    peca.value = parseInt(peca.value) + 1
+    }
+}
+
+function atualizaEstatisticas(peca){
+    
+
+    estatistica.forEach((elemento) =>{
+        console.log(elemento.textContent)
+        console.log(parseInt(elemento.textContent))
+        console.log(pecas[peca])
+        console.log([elemento.dataset.estatistica])
+        
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca] [elemento.dataset.estatistica]
+       
+    }) 
+    
 }
